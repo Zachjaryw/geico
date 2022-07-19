18ef1f788b4031fb9deeb555099f1d9479931a27
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-07-19 09:48:44 -0700

    Update geico.py

--- geico.py
+++ geico.py
@@ -4,9 +4,56 @@ st.title('GEICO Claims Comprehensive Attourney Information Check-List')
 
 claim_number = st.text_input('Enter 16 digit claim number',key = 0)
 
-state = st.select_box(['Washington','Oregon'],key = 1)
+state = st.selectbox(['Washington','Oregon'],key = 1)
 
-if state == 'Oregon':
-  st.write('Oregon')
-elif state == 'Washington':
-  st.write('Washington')
+with st.form():
+  q1 = st.checkbox('No Coverage Concerns?',False)
+  if state == 'Washington':
+    q2 = st.checkbox('Any amount of liability accepted?',False)
+    q3 = st.checkbox('No indication of big damages?',False)
+    q4 = st.checkbox("2 or less IP's?", False)
+    q5 = st.checkbox('No Reports of DUI or other agg liability?',False)
+    st.text('No anticipated biling through:')
+    harborview = st.checkbox('Harborview',False)
+    Swedish = st.checkbox('Swedish',False)
+    Providence = st.checkbox('Providence',False)
+    Virginia_mason = st.checkbox('Virginia Mason',False)
+    Valley_medical_center = st.checkbox('Valley Medical Center',False)
+    hospital = ['Harborview','Swedish','Providence','Virginia Mason','Valley Medical Center']
+    q6 = [harborview,Swedish,Providence,Virginia_mason,Valley_medical_center]
+  elif state == 'Oregon':
+    q2 = st.checkbox('Accepted 50% liability or over?',False)
+    q3 = st.checkbox('No indiation of big damages?',False)
+    q4 = st.checkbox("2 or less IP's?",False)
+    q5 = st.checkbox('No reports of DUI or other agg liability?',False)
+    q6 = st.checkbox('Consider if NPNP would apply',False)
+    q7 = st.checkbox('PIP unlikely to exhaust?',False)
+  submit = st.form_submit_button()
+
+if submit == True:
+  if state == 'Washington':
+    st.write(f'''
+    Claim Number: {claim_number}
+    State: {state}
+    No Coverage Concerns?: {q1}
+    Any amount of liability accepted?: {q2}
+    No indication of big damages?: {q3}
+    2 or less IP's?: {q4}
+    No Reports of DUI or other agg liability?: {q5}
+    ''')
+    if True in q6:
+      st.write(f'Anticipated billing through {hospital[q6.index(True)]}')
+  elif state == 'Oregon':
+    st.write(f'''
+    Claim Number: {claim_number}
+    State: {state}
+    No Coverage Concerns?: {q1}
+    Accepted 50% liability or over?: {q2}
+    No indiation of big damages?: {q3}
+    2 or less IP's?: {q4}
+    No reports of DUI or other agg liability?: {q5}
+    Consider if NPNP would apply: {q6}
+    PIP unlikely to exhaust?: {q7}
+    ''')
+    
+ 
