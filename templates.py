abcfb6603d258d90f241f302ebc1183643a32dc3
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-07-22 13:36:50 -0700

    Update templates.py

--- templates.py
+++ templates.py
@@ -12,11 +12,14 @@ templates = format()
 
 a = {'Question':['How are you?','Are you a human?','How many?'],
     'Type':['text_input','checkbox','number_input'],
-    'Addons':['',',value = False',',min_value = 1, max_value = 10,step = 1']}
+    'Addons':['',',value = False',',min_value = 1, max_value = 10,step = 1'],
+    'Condition':[True,True,q2]}
 
 responses = []
 for i in range(len(a['Question'])):
-  exec(f"q{i} = st.{a['Type'][i]}('{a['Question'][i]}',key = {i}{a['Addons'][i]})")
+  exec(f:""" 
+if {a['Condition'][i]}:
+    q{i} = st.{a['Type'][i]}('{a['Question'][i]}',key = {i}{a['Addons'][i]})""")
   exec(f'responses.append(q{i})')
   
 if st.button('Submit'):
