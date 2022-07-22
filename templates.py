6811f28fdb2cd9a3c88f1657b2db3a537a07501b
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-07-22 13:28:48 -0700

    Update templates.py

--- templates.py
+++ templates.py
@@ -7,4 +7,18 @@ st.title('GEICO TCR II Templates')
 
 templates = format()
 
-st.write(templates)
+#st.write(templates)
+
+
+a = {'Question':['How are you?','Are you a human?','How many?'],
+    'Type':['text_input','checkbox','number_input'],
+    'Addons':['',',value = False',',min = 1, max = 10']}
+
+responses = []
+for i in range(len(a['Question'])):
+  exec(f"q{i} = st.{a['Type'][i]}({a['Question'][i]},key = {i}{a['Addons'][i]})")
+  exec(f'responses.append(q{i})')
+  
+if st.button('Submit'):
+  for i in range(len(a['Question'])):
+    exec(f"st.write({a['Question'][i]}: {responses[i]})")
