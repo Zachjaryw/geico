89863249de476cecc9668fe5be73d7f0c67848ee
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-07-24 11:46:35 -0700

    Update templates.py

--- templates.py
+++ templates.py
@@ -7,13 +7,18 @@ templates = format()
 template = st.selectbox('Select which template you would like to use:',list(templates.keys()))
 responses = []
 for i in range(len(templates[template]['Question'])):
+  reverse == False
   if templates[template]['Condition'][i] == True:
     check = True
   else:
-    check = responses[int(templates[template]['Condition'][i])]
+    if 'N' in templates[template]['Condition'][i]:
+      reverse == True
+      check = responses[int(templates[template]['Condition'][i][1:])]
+    else:
+      check = responses[int(templates[template]['Condition'][i])]
   st.write(check)
   st.write(responses)
-  if check == True or check == 'Y':
+  if check == True or check == 'Y' or (reverse == True and check == 'N'):
     exec(f"""q{i} = st.{templates[template]['Type'][i]}('{templates[template]['Question'][i]}',key = {i}{templates[template]['Addons'][i]})""")
     exec(f"""
 if q{i} == True:
