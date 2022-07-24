0b34f4222631e047fcf1947485b5a9297a376b4c
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-07-24 11:14:55 -0700

    Update templates.py

--- templates.py
+++ templates.py
@@ -7,21 +7,18 @@ templates = format()
 template = st.selectbox('Select which template you would like to use:',list(templates.keys()))
 responses = []
 for i in range(len(templates[template]['Question'])):
-  exec(f""" 
-if {templates[template]['Condition'][i]}:
-    q{i} = st.{templates[template]['Type'][i]}('{templates[template]['Question'][i]}',key = {i}{templates[template]['Addons'][i]})
+  check = exec(templates[template]['Condition'][i])
+  if check:
+    exec(f"""q{i} = st.{templates[template]['Type'][i]}('{templates[template]['Question'][i]}',key = {i}{templates[template]['Addons'][i]})""")
     if q{i} == True:
      q{i} = 'Y'
     elif q{i} == False:
       q{i} = 'N'
     exec(f'responses.append(q{i})')
-    """)
 else:
     exec(f'responses.append(np.nan)')
     
 if st.button('Submit'):
   for i in range(len(templates[template]['Question'])):
-    exec(f"""
-if not('{responses[i]}' == '{np.nan}'):
-    st.write('{templates[template]['Question'][i]}: {responses[i]}')
-    """)
+    if not(responses[i] == np.nan):
+    exec(f"""st.write('{templates[template]['Question'][i]}: {responses[i]}')""")
