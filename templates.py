6660f9bf107af5b3099139e65661c8bc554d06fa
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-07-24 11:16:29 -0700

    Update templates.py

--- templates.py
+++ templates.py
@@ -10,11 +10,13 @@ for i in range(len(templates[template]['Question'])):
   check = exec(templates[template]['Condition'][i])
   if check:
     exec(f"""q{i} = st.{templates[template]['Type'][i]}('{templates[template]['Question'][i]}',key = {i}{templates[template]['Addons'][i]})""")
+    exec(f"""
     if q{i} == True:
      q{i} = 'Y'
     elif q{i} == False:
       q{i} = 'N'
     exec(f'responses.append(q{i})')
+    """)
 else:
     exec(f'responses.append(np.nan)')
     
