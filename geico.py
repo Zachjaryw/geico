1d9a5ac1caece4b631d7d6721e0d83f34c945a97
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-07-24 12:45:43 -0700

    Update geico.py

--- geico.py
+++ geico.py
@@ -19,7 +19,7 @@ def reset(dbx):
 try:
   dbx = initializeToken(st.secrets.Token.token)
 except:
-  token = get_new_auth_token(st.secrets.Token.key,st.secrets.Token.secret)
+  token = Dropbox_Setup.get_new_auth_token(st.secrets.Token.key,st.secrets.Token.secret)
   dbx = initializeToken(token)
   
 data = fromDBX(dbx,st.secrets.filepath.rentonCAIC)
