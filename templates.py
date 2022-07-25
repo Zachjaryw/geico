58fd2ffb4001e9571ebba350565afe4593788a47
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-07-25 12:05:55 -0700

    Update templates.py

--- templates.py
+++ templates.py
@@ -11,6 +11,7 @@ for i in range(len(templates[template]['Question'])):
   if templates[template]['Condition'][i] == True:
     check = True
   else:
+    st.write(check)
     if 'N' in str(templates[template]['Condition'][i]):
       reverse = True
       check = responses[int(templates[template]['Condition'][i][1:])]
